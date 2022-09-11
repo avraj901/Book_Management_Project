e73e1f16824ddd233896d69d636cb4b529369ab6
avraj901 <avraj901@gmail.com>
2022-09-11 12:20:58 +0530

    code base

---
+++ Book_Management_System/Reader_MS/src/main/java/com/reader/ReaderMsApplication.java
@@ -0,0 +1,13 @@
+package com.reader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReaderMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReaderMsApplication.class, args);
+	}
+
+}
